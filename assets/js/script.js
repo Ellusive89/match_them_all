d1b52a1f7ab0ab068cf3d920be95a9fa344fa0ba
Ellusive89 <eva.nagrodzka@gmail.com>
2023-03-24 23:42:52 +0000

    Add item array for game-cards to js.script

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// card items array
+const cardItm = [
+    {name: "anna", image: "anna.png"},
+    {name: "ariel", image: "ariel.png"},
+    {name: "aurora", image: "aurora.png"},
+    {name: "belle", image: "belle.png"},
+    {name: "cinderella", image: "cinderella"},
+    {name: "elsa", image: "elsa.png"},
+    {name: "jasmine", image: "jasmine.png"},
+    {name: "merida", image: "merida.png"},
+    {name: "mirabel", image: "mirabel.png"},
+    {name: "moana", image: "moana.png"},
+    {name: "mulan", image: "mulan.png"},
+    {name: "pocahontas", image: "pocahontas.png"},
+    {name: "rapunzel", image: "rapunzel.png"},
+    {name: "snow_white", image: "snow_white.png"},
+    {name: "tiana", image: "tiana.png"}
+];
